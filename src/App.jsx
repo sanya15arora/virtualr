43d39f66be60331c811e6e47a104a28e6f9150ca
Sanya's Mac <mac@Macs-MacBook-Pro.local>
2024-11-17 12:19:39 +0530

    Landing page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/Featuresection'
+import WorkFlow from './components/WorkFlow'
+import PricingSection from './components/PricingSection'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+export const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
+        <PricingSection />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
